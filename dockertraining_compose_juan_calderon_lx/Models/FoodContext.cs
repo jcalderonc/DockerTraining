a461710395e374fc2b5baf2123085dabaee939f0
Juan Carlos Calderon Castro <juan.calderon@agilethought.com>
2020-05-04 04:14:50 -0500

    Unable to continue because I have an issue with the MYSQL container, please take a look at the readme file for details.

---
+++ dockertraining_compose_juan_calderon_lx/Models/FoodContext.cs
@@ -0,0 +1,11 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+namespace dockertraining_compose_juan_calderon_lx.Models
+{
+    public class FoodContext : DbContext
+    {
+        public DbSet<Food> Foods { get; set; }
+
+        public FoodContext(DbContextOptions options) : base(options) { }
+    }
+}
